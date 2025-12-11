c294e47f41f285dd04f048b08145a99c387eb59f
rain-ameovO <aj4n24@soton.ac.uk>
2025-12-11 22:35:05 +0800

    Add files via upload

---
+++ ethereum_scraper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+db_name="crypto_db"
+table_name="crypto_rate"
+
+ethereum_url="https://coinmarketcap.com/currencies/ethereum/"
+temp_file="ethereum_price.html"
+curl -s "$ethereum_url" > "$temp_file"
+
+price=$(grep -oP '<span class="sc-65e7f566-0 WXGwg base-text" data-test="text-cdp-price-display">\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
+low_price=$(grep -oP 'label">Low</div><span>\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
+high_price=$(grep -oP 'label">High</div><span>\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
+
+price=${price//,/}
+low_price=${low_price//,/}
+high_price=${high_price//,/}
+current_time="`date "+%Y-%m-%d %H:%M:%S"`"
+
+/opt/lampp/bin/mysql -u root -D "$db_name" -e \
+"INSERT INTO $table_name (api_id, price, price_time, low_price, high_price) VALUES ('ethereum', '$price', NOW(), '$low_price', '$high_price');"
+
+echo "| $current_time | Ethereum price: $ $price | 24H Low: $ $low_price | 24H High: $high_price |"
