7e929cd7270440726629acef4838db99e27ecb47
rain-ameovO <aj4n24@soton.ac.uk>
2025-12-12 07:58:19 +0800

    Add files via upload

---
+++ bch_scraper.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+db_name="crypto_db"
+table_name="crypto_rate"
+
+bch_url="https://coinmarketcap.com/currencies/bitcoin-cash/"
+temp_file="bch_price.html"
+curl -s "$bch_url" > "$temp_file"
+
+if [ $? -ne 0 ]; then
+        echo "Failed to fetch website. Please check if the network or the website is down."
+        exit 1
+    fi
+
+price=$(grep -oP '<span class="sc-65e7f566-0 WXGwg base-text" data-test="text-cdp-price-display">\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
+low_price=$(grep -oP 'label">Low</div><span>\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
+high_price=$(grep -oP 'label">High</div><span>\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
+
+price=${price//,/}
+low_price=${low_price//,/}
+high_price=${high_price//,/}
+current_time="`date "+%Y-%m-%d %H:%M:%S"`"
+
+if [ -z "$price" ] || [ -z "$low_price" ] || [ -z "$high_price" ] ; then
+        echo "Failed to get Bitcoin-Cash price."
+	exit 1
+    fi
+
+/opt/lampp/bin/mysql -u root -D "$db_name" -e \
+"INSERT INTO $table_name (api_id, price, price_time, low_price, high_price) VALUES ('bitcoincash', '$price', NOW(), '$low_price', '$high_price');"
+
+if [ $? -ne 0 ]; then
+        echo "Failed to insert data into database."
+        exit 1
+    fi
+
+echo "| $current_time | Bitcoin-Cash price: $ $price | 24H Low: $ $low_price | 24H High: $high_price |"
+
+rm -f "$temp_file"
