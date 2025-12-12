f17d088d60c6f93f5bef60a03e2bbd05a3ec3bf6
rain-ameovO <aj4n24@soton.ac.uk>
2025-12-12 08:12:28 +0800

    Add files via upload

--- bnb_scraper.sh
+++ bnb_scraper.sh
@@ -7,6 +7,11 @@ bnb_url="https://coinmarketcap.com/currencies/bnb/"
 temp_file="bnb_price.html"
 curl -s "$bnb_url" > "$temp_file"
 
+if [ $? -ne 0 ]; then
+        echo "Failed to fetch website. Please check if the network or the website is down."
+        exit 1
+    fi
+
 price=$(grep -oP '<span class="sc-65e7f566-0 WXGwg base-text" data-test="text-cdp-price-display">\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
 low_price=$(grep -oP 'label">Low</div><span>\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
 high_price=$(grep -oP 'label">High</div><span>\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
@@ -16,9 +21,19 @@ low_price=${low_price//,/}
 high_price=${high_price//,/}
 current_time="`date "+%Y-%m-%d %H:%M:%S"`"
 
+if [ -z "$price" ] || [ -z "$low_price" ] || [ -z "$high_price" ] ; then
+        echo "Failed to get Binance Coin price."
+	exit 1
+    fi
+
 /opt/lampp/bin/mysql -u root -D "$db_name" -e \
 "INSERT INTO $table_name (api_id, price, price_time, low_price, high_price) VALUES ('binancecoin', '$price', NOW(), '$low_price', '$high_price');"
 
-echo "| $current_time | BNB price: $ $price | 24H Low: $ $low_price | 24H High: $high_price |"
+if [ $? -ne 0 ]; then
+        echo "Failed to insert data into database."
+        exit 1
+    fi
+
+echo "| $current_time | Binance Coin price: $ $price | 24H Low: $ $low_price | 24H High: $high_price |"
 
 rm "$temp_file"
--- solana_scraper.sh
+++ solana_scraper.sh
@@ -7,6 +7,11 @@ solana_url="https://coinmarketcap.com/currencies/solana/"
 temp_file="solana_price.html"
 curl -s "$solana_url" > "$temp_file"
 
+if [ $? -ne 0 ]; then
+        echo "Failed to fetch website. Please check if the network or the website is down."
+        exit 1
+    fi
+
 price=$(grep -oP '<span class="sc-65e7f566-0 WXGwg base-text" data-test="text-cdp-price-display">\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
 low_price=$(grep -oP 'label">Low</div><span>\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
 high_price=$(grep -oP 'label">High</div><span>\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
@@ -16,8 +21,18 @@ low_price=${low_price//,/}
 high_price=${high_price//,/}
 current_time="`date "+%Y-%m-%d %H:%M:%S"`"
 
+if [ -z "$price" ] || [ -z "$low_price" ] || [ -z "$high_price" ] ; then
+        echo "Failed to get Solana price."
+	exit 1
+    fi
+
 /opt/lampp/bin/mysql -u root -D "$db_name" -e \
 "INSERT INTO $table_name (api_id, price, price_time, low_price, high_price) VALUES ('solana', '$price', NOW(), '$low_price', '$high_price');"
+
+if [ $? -ne 0 ]; then
+        echo "Failed to insert data into database."
+        exit 1
+    fi
 
 echo "| $current_time | Solana price: $ $price | 24H Low: $ $low_price | 24H High: $high_price |"
 
