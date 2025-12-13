2534ebdc68f48dc207488cbe3645cf3bb1a10ef0
rain-ameovO <aj4n24@soton.ac.uk>
2025-12-13 18:26:41 +0800

    Add files via upload

--- bitcoin_scraper.sh
+++ bitcoin_scraper.sh
@@ -3,37 +3,44 @@
 db_name="crypto_db"
 table_name="crypto_rate"
 
-bitcoin_url="https://coinmarketcap.com/currencies/bitcoin/"
+bitcoin_url="https://coinmarketcap.com/currencies/bitcoin/" 
 temp_file="bitcoin_price.html"
-curl -s "$bitcoin_url" > "$temp_file"
+curl -s "$bitcoin_url" > "$temp_file" # Curl website and save to a temporary file
 
+# Check if network and website is available
 if [ $? -ne 0 ]; then
         echo "Failed to fetch website. Please check if the network or the website is down."
         exit 1
     fi
 
+# Collect data
 price=$(grep -oP '<span class="sc-65e7f566-0 WXGwg base-text" data-test="text-cdp-price-display">\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
 low_price=$(grep -oP 'label">Low</div><span>\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
 high_price=$(grep -oP 'label">High</div><span>\$\K[0-9,]+\.[0-9]+' "$temp_file" | head -n 1)
 
+# Remove ',' from the data
 price=${price//,/}
 low_price=${low_price//,/}
 high_price=${high_price//,/}
 current_time="`date "+%Y-%m-%d %H:%M:%S"`"
 
+# Check if all data is colected
 if [ -z "$price" ] || [ -z "$low_price" ] || [ -z "$high_price" ] ; then
         echo "Failed to get Bitcoin price."
 	exit 1
     fi
 
+# Insert data into sql table
 /opt/lampp/bin/mysql -u root -D "$db_name" -e \
 "INSERT INTO $table_name (api_id, price, price_time, low_price, high_price) VALUES ('bitcoin', '$price', NOW(), '$low_price', '$high_price');"
 
+# Check if data is successfully inserted into the table
 if [ $? -ne 0 ]; then
         echo "Failed to insert data into database."
         exit 1
     fi
 
-echo "| $current_time | Bitcoin price: $ $price | 24H Low: $ $low_price | 24H High: $high_price |"
+echo "| $current_time | Bitcoin price: $ $price | 24H Low: $ $low_price | 24H High: $ $high_price |"
 
+# Remove temporary file
 rm "$temp_file"
